26d7abfd3cf71ba349ea1ac5aee38e9d59a97f59
Sergiu Drahnea <s.drahnea@gmail.com>
2022-12-19 20:24:27 +0200

    intendation

--- src/main/java/com/designpatterns/creational/singleton/Singleton.java
+++ src/main/java/com/designpatterns/creational/singleton/Singleton.java
@@ -2,7 +2,8 @@ package com.designpatterns.creational.singleton;
 
 public class Singleton {
 
-    private static Singleton instance; public String value;
+    private static Singleton instance;
+    public String value;
 
     private Singleton(String value) {
         this.value = value;
