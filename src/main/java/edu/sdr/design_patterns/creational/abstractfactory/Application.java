bb055d0a6318314e9d3e5bccb544593a5b193dc6
Sergiu Drahnea <s.drahnea@gmail.com>
2023-01-01 17:53:40 +0200

    small update

--- src/main/java/edu/sdr/design_patterns/creational/abstractfactory/Application.java
+++ src/main/java/edu/sdr/design_patterns/creational/abstractfactory/Application.java
@@ -1,4 +1,4 @@
-package edu.sdr.designpatterns.creational.abstractfactory;
+package edu.sdr.design_patterns.creational.abstractfactory;
 
 /**
  * Factory users don't care which concrete factory they use since they work with
