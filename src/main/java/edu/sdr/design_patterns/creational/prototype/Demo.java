20401d9c7e2962d5ac27517aba669e1c19b47a69
Sergiu Drahnea <s.drahnea@gmail.com>
2023-01-29 11:35:30 +0200

    prototype refactor

--- src/main/java/edu/sdr/design_patterns/creational/prototype/Demo.java
+++ src/main/java/edu/sdr/design_patterns/creational/prototype/Demo.java
@@ -1,4 +1,4 @@
-package edu.sdr.designpatterns.creational.prototype;
+package edu.sdr.design_patterns.creational.prototype;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/edu/sdr/design_patterns/creational/prototype/Rectangle.java
+++ src/main/java/edu/sdr/design_patterns/creational/prototype/Rectangle.java
@@ -1,4 +1,4 @@
-package edu.sdr.designpatterns.creational.prototype;
+package edu.sdr.design_patterns.creational.prototype;
 
 public class Rectangle extends Shape {
     public int width;
